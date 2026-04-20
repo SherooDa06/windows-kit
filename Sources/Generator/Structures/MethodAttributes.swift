173b5525f8c5385796662b73eb12764e695d461a
SherooDa@outlook.com <SherooDa@outlook.com>
2026-04-20 22:54:33 +1000

    Added the VTableLayout and OverrideAttributes structs

--- Sources/Generator/Structures/MethodAttributes.swift
+++ Sources/Generator/Structures/MethodAttributes.swift
@@ -11,10 +11,35 @@ struct MethodAttributes {
         case `public` = 0x0006
     }
 
+    // How wrong does this look? 
+    // I wrote it with OptionSet since 'static' and 'final' are not mutually exclusive keywords.
+    // Meanwhile, I also wrote it as a Maskable as it has 'vtableLayoutMask' that retrieves vtable atributes
+    // Furthermore, there cannot be a static virutal function, 
+    struct VTableLayout: OptionSet, Maskable {
+               let rawValue: UInt16
+        static let mask: UInt16 = 0x0100
+
+        static let `static` = Self(rawValue: 0x0010)
+        static let `final` = Self(rawValue: 0x0020)
+        static let virtual = Self(rawValue: 0x0040)
+        static let hideBySig = Self(rawValue: 0x0080)
+    }
+
     // I don't know what this enum should be calles, so I'm just calling this SlotInit for now
     enum SlotInit: UInt16 {
         case reuseSlot = 0x0000
         case newSlot = 0x0100
+    }
+
+    struct OverrideAttributes: OptionSet {
+        let rawValue: UInt16
+
+        // I know that 'abstract' methods must be overriden, and 'strict' methods can only be overriden if accessible
+        // But I also heard that abstract methods cannot be private
+        // So I feel like this is a good implementation for methods that can be overriden
+        static let strict = Self(rawValue: 0x0200)
+        static let abstract = Self(rawValue: 0x0400)
+        static let specialName = Self(rawValue: 0x0800)
     }
 
     struct InteropAttributes: OptionSet {
